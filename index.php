1397c81313c84ced38cb00f37851503920005570
Vince <vincethedev@gmail.com>
2024-07-26 02:10:12 +0800

    First commit

---
+++ index.php
@@ -0,0 +1,323 @@
+<!DOCTYPE html>
+<!-- saved from url=(0041)https://unilympics.com/stg-unilympics.php -->
+<html lang="en" class=" qfyooa idc0_350">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- <script>
+      (function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l != "dataLayer" ? "&l=" + l : "";
+        j.async = true;
+        j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+        f.parentNode.insertBefore(j, f);
+      })(window, document, "script", "dataLayer", "GTM-TRXTDD3B");
+    </script> -->
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Unifi - Unilympics Game</title>
+    <link rel="icon" href="https://unifi.com.my/themes/unifi/favicon.ico" type="image/vnd.microsoft.icon">
+    <link rel="shortcut icon" href="https://unifi.com.my/themes/unifi/favicon.ico" type="image/vnd.microsoft.icon">
+
+    <!-- Open Graph Metadata -->
+    <meta property="og:title" content="Unifi - Unilympics Game">
+    <meta property="og:description" content="PLAY THE UNILYMPICS GAME AND PROVE YOU&#39;VE GOT THE FASTEST, SLICKEST FINGERS">
+    <meta property="twitter:image" content="https://unilympics.com/assets/MetaHeader-Unilympics_v2.jpg">
+    <meta property="og:image" content="https://unilympics.com/assets/MetaHeader-Unilympics_v2.jpg">
+    <meta property="og:url" content="https://unilympics.com">
+    <meta property="og:type" content="website">
+
+    <!-- CSS for styling -->
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./custom-style.css">
+</head>
+
+<body>
+    <!-- <noscript
+      ><iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-TRXTDD3B"
+        height="0"
+        width="0"
+        style="display: none; visibility: hidden"
+      ></iframe
+    ></noscript> -->
+
+    <div class="desktop-screen">
+        <div class="main-desktop">
+            <div class="main-banner-1">
+                <!-- <a
+            href="https://unifi.com.my?utm_source=microsite&utm_medium=um_logo&utm_campaign=unilympics_2024"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="desktop-unifi-logo"
+          >
+            <img src="./assets/unifi_logo_v2.png" alt=""
+          /></a> -->
+                <img class="new_play_btn" id="deskstop-play-btn" src="./assets/play_btn_v2.png" alt="">
+                <img class="new_masthead_title" src="./assets/masthead_title_v11.png" alt="">
+                <img class="main-banner-1-img" src="./assets/desktop_hero_banner_v1.png" alt="">
+            </div>
+            <!-- <div class="main-banner-2">
+          <img
+            class="deskstop-masthead-img"
+            src="./assets/masthead_v2.png"
+            alt=""
+          />
+          <img
+            class="deskstop-masthead-title-img"
+            src="./assets/masthead_desktop_v3.png"
+            alt=""
+          />
+          <img
+            class="deskstop-play-btn"
+            id="deskstop-play-btn"
+            src="./assets/play_btn_v1.png"
+            alt=""
+          />
+        </div> -->
+        </div>
+
+        <div class="info-desktop-wrapper">
+            <img src="./assets/usp-header-desktop-v2.png" class="usp-header-desktop" alt="">
+
+            <div class="info-desktop-details">
+                <div class="info-desktop-more-details">
+                    <img src="./assets/infinity_v2.png" alt="">
+                    <p>Unlimited 5G + 4G Data</p>
+                </div>
+
+                <div class="info-desktop-more-details">
+                    <img src="./assets/data5G_v2.png" alt="">
+                    <p>
+                        Uncapped 5G Speed, <br>
+                        All-Day Long
+                    </p>
+                </div>
+
+                <div class="info-desktop-more-details">
+                    <img class="info-desktop-more-details-openSignal-img" src="./assets/openSignal_v2.png" alt="">
+                    <p>
+                        World's No.1 for 5G <br>
+                        Download Speed
+                    </p>
+                    <p class="openSignal_desktop">
+                        Opensignal Awards - 5G Global Mobile Network Experience Report,
+                        <br>
+                        Group 1, October 2023
+                    </p>
+                </div>
+            </div>
+
+            <!-- <a
+          href="https://unifi.com.my/mobile?utm_source=microsite&utm_medium=mobile_usp&utm_campaign=unilympics_2024"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="find-out-more-btn-desktop"
+          >Find Out More</a
+        > -->
+
+            <div class="more-btn-container-desktop">
+                <a href="https://unifi.com.my/mobile?utm_source=microsite&amp;utm_medium=mobile_usp&amp;utm_campaign=unilympics_2024" target="_blank" rel="noopener noreferrer">
+                    <div class="arrow-btn-wrapper-desktop">
+                        <span style="font-weight: 600">Find out more</span>
+                        <i class="arrow-btn-desktop right-btn-desktop"></i>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="footer-banner-desktop">
+            <a href="https://unifi.com.my/tv/promotions/paris-2024?utm_source=microsite&amp;utm_medium=unifi_tv_banner&amp;utm_campaign=unilympics_2024" target="_blank" rel="noopener noreferrer">
+                <img src="./assets/footer-banner-desktop-v4.png" class="footer-banner-img" alt="">
+                <div class="footer-banner-text-wrapper">
+                    <img class="footer-banner-text-img" src="./assets/footer-banner-desktop-text-v1.png" alt="">
+                    <p class="footer-banner-text-2">
+                        Enjoy your complimentary access to watch Olympic Games Paris 2024
+                        on <br>
+                        Unifi Sports channels from 24 Jul - 12 Aug 2024. Download now!
+                    </p>
+                </div>
+            </a>
+        </div>
+
+        <div class="desktop-footer">
+            <div style="padding: 20px">
+                <p>*Terms and conditions apply.</p>
+                <p style="margin-top: 16px">
+                    *Users must use their own mobile data to access and play the game.
+                    The organiser is not responsible for any data charges incurred.
+                </p>
+                <p style="margin-top: 16px">
+                    Copyright © 2024 Telekom Malaysia Berhad. All rights reserved.
+                </p>
+            </div>
+            <img src="./assets/footer_img_v2.png" alt="" style="width: 8vw">
+        </div>
+    </div>
+
+    <div id="message_modal" class="modal">
+        <div class="modal-content">
+            <span class="close">×</span>
+            <p class="pop-up-message">
+                The UNI5G Unilympics is designed for mobile and features an optimised
+                user interface for one-hand play. For the best gameplay experience,
+                please use your smartphone.
+            </p>
+        </div>
+    </div>
+
+    <div class="mobile-screen">
+        <!--Nav bar-->
+        <div class="main">
+            <div class="logo-wrapper">
+                <a href="https://unifi.com.my/?utm_source=microsite&amp;utm_medium=um_logo&amp;utm_campaign=unilympics_2024" target="_blank" rel="noopener noreferrer">
+                    <img src="./assets/NEW/logo.png" alt=""></a>
+            </div>
+            <div class="masthead-wrapper">
+                <img class="masthead-logo" src="./assets/masthead_v2.png" alt="">
+                <img class="masthead-title" src="./assets/masthead_mobile_v4.png" alt="">
+            </div>
+        </div>
+
+        <? /*
+        <div class="play-wrapper">
+            <img src="./assets/mobile_hero_banner_new_v1.png" class="mobile-banner-new mobile-banner-new-class" alt="">\
+            <img src="./assets/masthead_new_v2.png" class="new_masthead_updated" alt="">
+            <img src="./assets/play_btn_v2.png" class="play_btn" id="play_btn" alt="">
+            <img src="./assets/masthead_new_title_v2.png" class="masthead_title_mobile" alt="">
+        </div>
+        */ ?>
+
+        <section class="main-section">
+            <div class="content-container">
+                <img src="./assets/NEW/uni5g-unilympics.png" alt="Unilympics" class="main-image" />
+                <div class="sub-section">
+                    <img src="./assets/NEW/headline.png" alt="Unilympics" class="headline" />
+                    <img src="./assets/NEW/subheadline.png" alt="Unilympics" class="subheadline" />
+
+                    <? /*
+                    <h1 class="title">BE THE NEXT <br />ONE HAND OLYMPIAN</h1>
+                    <p class="subheading">Play the unilympics game and prove you've got the fastest,
+                        slickest fingers in town!</p> 
+                        */ ?>
+
+                    <img src="./assets/play_btn_v2.png" alt="Call to Action" class="image-button">
+                </div>
+            </div>
+        </section>
+
+
+        <div class="info-wrapper">
+            <img class="info-title" src="./assets/second_text_v2.png" alt="">
+            <!-- <p class="info-title">ENJOY UNSTOPPABLE GAMING WITH UNIFI MOBILE</p> -->
+
+            <div>
+                <div class="info-details">
+                    <img class="white-icon" src="./assets/infinity_v1.png" alt="">
+                    <p>Unlimited 5G + 4G Data</p>
+                </div>
+
+                <div class="info-details">
+                    <img class="white-icon" src="./assets/data5G_v1.png" alt="">
+                    <p>
+                        Uncapped 5G Speed, <br>
+                        All-Day Long
+                    </p>
+                </div>
+
+                <div class="info-details">
+                    <img class="openSignalImg" src="./assets/openSignal_v1.png" alt="">
+                    <p>World's No.1 for 5G Download Speed</p>
+                    <p class="open_signal-desc">
+                        Opensignal Awards - 5G Global Mobile Network Experience Report,
+                        Group 1, October 2023
+                    </p>
+                </div>
+            </div>
+
+            <!-- <a
+          href="https://unifi.com.my/mobile?utm_source=microsite&utm_medium=mobile_usp&utm_campaign=unilympics_2024"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="find-out-more-btn"
+          >Find Out More</a
+        > -->
+
+            <div class="more-btn-container">
+                <a href="https://unifi.com.my/mobile?utm_source=microsite&amp;utm_medium=mobile_usp&amp;utm_campaign=unilympics_2024" target="_blank" rel="noopener noreferrer">
+                    <div class="arrow-btn-wrapper">
+                        <span style="font-weight: 600">Find out more</span>
+                        <i class="arrow right"></i>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div>
+            <div class="footer-banner">
+                <a href="https://unifi.com.my/tv/promotions/paris-2024?utm_source=microsite&amp;utm_medium=unifi_tv_banner&amp;utm_campaign=unilympics_2024" target="_blank" rel="noopener noreferrer">
+                    <img src="./assets/footer_banner_v4.png" class="footer-banner-img" alt="">
+                    <div class="footer-banner-text-wrapper">
+                        <img class="footer-banner-text-img" src="./assets/third_text_v2.png" alt="">
+                        <p class="footer-banner-text-2">
+                            Enjoy your complimentary access to watch Olympic Games Paris
+                            2024 on Unifi Sports channels from 24 Jul - 12 Aug 2024.
+                            Download now!
+                        </p>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="footer">
+            <div style="padding: 14px">
+                <p>*Terms and conditions apply.</p>
+                <p>
+                    *Users must use their own mobile data to access and play the game.
+                    The organiser is not responsible for any data charges incurred.
+                </p>
+                <p>
+                    Copyright © 2024 Telekom Malaysia Berhad. All rights reserved.
+                </p>
+            </div>
+            <img src="./assets/footer_img_v2.png" alt="" style="width: 29vw; height: 7vw">
+        </div>
+    </div>
+
+
+    <script>
+        const play_btn = document.getElementById("play_btn");
+        play_btn.addEventListener("click", () => {
+            window.open(
+                "https://unilympics.com/play/index.html",
+                "_blank",
+                "noopener,noreferrer"
+            );
+        });
+
+        let modal = document.getElementById("message_modal");
+        let btn = document.getElementById("deskstop-play-btn");
+        let span = document.getElementsByClassName("close")[0];
+
+        btn.onclick = function() {
+            modal.style.display = "block";
+        };
+
+        span.onclick = function() {
+            modal.style.display = "none";
+        };
+
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        };
+    </script>
+
+</body>
+
+</html>
